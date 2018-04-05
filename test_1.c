f0ea800b605e6fda69b5510c1d8a9b9abaf2b826
Tham Chee Peng <cheepeng.tham@liteon.com>
2018-04-05 15:45:25 +0800

    add tests

---
+++ test_1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <hello.h>
+
+
+int main() {
+  
+  int ret = -1;
+  if (ret<0)
+  {
+ 	printHello("I are unlucky");
+	return(-1);
+	exit(1);
+  }
+  else
+  {
+	printHello("I am fantastic");
+	return(0);
+  }
+	
+  exit(0);
+}
